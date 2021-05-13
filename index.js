957f53374ab8399dd7ca040fa4dcd823b705ce27
Miq <navasardianmichael@gmail.com>
2021-05-13 13:29:07 +0400

    da[wwad

--- index.js
+++ index.js
@@ -59,7 +59,7 @@ function BoomCalendar(props) {
   }
 
   function getViewPlugins(views) {
-    if (!views || !views.length) return APP_DEFAULTS.plugins;
+    if (!views || !views.length) return 'month';
     return views.filter(function (view) {
       if (!VIEW_PLUGINS.hasOwnProperty(view)) {
         console.error('provided view type ' + view + ' is not listed in permitted views');
